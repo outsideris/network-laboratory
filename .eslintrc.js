cc53e1c866db07751483165fb4b96e4ff21dbb9d
Outsider <outsideris@gmail.com>
2021-08-02 22:30:49 +0900

    introduce prettier
    Signed-off-by: Outsider <outsideris@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,10 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
