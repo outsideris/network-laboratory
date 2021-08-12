8829a32a47e9fe423cccf4f5e15f6e2bdd0ad899
Outsider <outsideris@gmail.com>
2021-08-12 22:45:03 +0900

    use one server file again
    Signed-off-by: Outsider <outsideris@gmail.com>

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const path = require('path');
 const AutoLoad = require('fastify-autoload');
 
 module.exports = async function (fastify, opts) {
-  console.log("It's a leader");
+  if (process.env.ROLE === 'leader') {
+    console.log('Server started as leader');
+  } else {
+    console.log('Server started as follower');
+  }
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
